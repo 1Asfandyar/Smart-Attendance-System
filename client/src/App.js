38a369a67ce92d949409755bb8c40783a0df772b
Abdur Rahman <khalil.ahmad1547@gmail.com>
2022-03-02 00:53:11 +0500

    routes for login & dashboard is added.
    simple url based routing is added.
    NEXT: add redirection to dashboard after successful login.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 import "./App.css";
+import Auth from "./Pages/Auth";
+import DashBoard from "./Pages/DashBoard";
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Auth />} />
+          <Route path="/dashboard" element={<DashBoard />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
