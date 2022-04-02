9ef4a38e81f9a7386ca815deaffaab8eee0e73f4
Abdur Rahman <khalil.ahmad1547@gmail.com>
2022-04-03 04:29:08 +0500

    migrated to vite tool.
    * npm start was very slow to display changes so moved to vite.
    
    * it uses modules and display changes as it is saved.

--- client/src/Pages/DashBoard.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const DashBoard = () => {
-  return <div>DashBoard Page</div>;
-};
-
-export default DashBoard;
--- client/src/Utils/fetchData.js
+++
@@ -1,7 +0,0 @@
-const fetchData = async (url) => {
-  let data = await fetch(url);
-  data = await data.json();
-  return data;
-};
-
-export default fetchData;
