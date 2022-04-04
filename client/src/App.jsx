4a5a328ea862c28586f91dd906f2407610599c08
Abdur Rahman <khalil.ahmad1547@gmail.com>
2022-04-05 02:12:43 +0500

    profile page is added.
    * simple profile page is added, next step is to protect this route.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,14 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./Components/Login";
 import DashBoard from "./Components/DashBoard";
 import Error from "./Components/Error";
+import Profile from "./Components/Profile";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/dashboard" element={<DashBoard />} />
+      <Route path="/profile" element={<Profile />} />
       <Route path="*" element={<Error />} />
     </Routes>
   );
