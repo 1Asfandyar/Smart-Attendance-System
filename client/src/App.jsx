f4d0ee6fbef3c668367551a9c3ee7d0b1950ae45
Abdur Rahman <khalil.ahmad1547@gmail.com>
2022-04-05 05:23:16 +0500

    logic of login user is implemented.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,15 +6,20 @@ import Login from "./Components/Login";
 import DashBoard from "./Components/DashBoard";
 import Error from "./Components/Error";
 import Profile from "./Components/Profile";
+import { AuthProvider } from "./Components/utils/auth";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/dashboard" element={<DashBoard />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="*" element={<Error />} />
-    </Routes>
+    <>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<DashBoard />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </AuthProvider>
+    </>
   );
 }
 
--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "./utils/auth";
 
 function Login() {
-  return <div>Login Page</div>;
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const auth = useAuth();
+  const navigate = useNavigate();
+
+  const handleUserChange = (e) => {
+    setUserName(e.target.value);
+  };
+
+  const handlePassChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    console.log(`User Name: ${userName}`);
+    console.log(`Password: ${password}`);
+
+    // if success
+    auth.login(userName);
+    // redirect to dashboard
+    navigate("/dashboard", { replace: true });
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      User Name: <input type="text" onChange={handleUserChange} />
+      Password <input type="password" onChange={handlePassChange} />
+      <button type="submit" onClick={handleSubmit}>
+        Login
+      </button>
+    </div>
+  );
 }
 
 export default Login;
