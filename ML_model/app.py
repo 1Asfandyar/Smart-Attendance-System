36a2f97e14e19dc7e0babdeaf5d23b42619c3b30
Abdur Rahman <khalil.ahmad1547@gmail.com>
2022-05-04 15:48:14 +0500

    a basic flask api is created.
    * at this point app uses env variables
    * and a simple root route is implemented.

---
+++ ML_model/app.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask
+# from home import home_bp
+
+load_dotenv()
+PORT = os.getenv('PORT')
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return "Home page"
+# app.register_blueprint(home_bp, url_prefix='/home')
+
+# use_reloader → When use_reloader is set to True , 
+# the server will automatically restart when code changes. 
+# Defaults to False
+app.run(host='0.0.0.0', port=PORT, use_reloader=True)
