f2a5d18d5deee4fe603af4858b44b0b7a77728bf
Abdur Rahman <khalil.ahmad1547@gmail.com>
2022-05-04 17:10:41 +0500

    a function which can decode base64 encoded image.
    * at this point it is assumed that image would be sent as base64
    encode string.
    
    * so to handle this base4 encode image to be usefull for ML modle it
    convets base64 string to image.

---
+++ ML_model/decode_img.py
@@ -0,0 +1,8 @@
+import base64
+
+def decode(str_img:str):
+    # this method take base64 incoded image and returns the data
+    # which is required for decoding the image.
+    # encoded string would be like
+    # data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAASABIAAD/
+    return base64.b64decode(str_img.split(",")[1])
