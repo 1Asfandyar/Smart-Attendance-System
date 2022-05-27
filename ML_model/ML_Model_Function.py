4f7bf15cb474fb987579684109182c34840490b9
Abdur Rahman <khalil.ahmad1547@gmail.com>
2022-05-27 23:22:13 +0500

    py file for ml model functions updated.
    * ML_Model_Function.ipynb is exported as updated_ml_function.py file.
    
    * renmad to ML_Model_Function.py file

--- ML_model/ML_Model_Function.py
+++ ML_model/ML_Model_Function.py
@@ -1,3 +1,19 @@
+# %% [markdown]
+# <a href="https://colab.research.google.com/github/1Asfandyar/Smart-Attendance-System/blob/AI-model/ML_model/ML_Model_Function.ipynb" target="_parent"><img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/></a>
+
+# %% [markdown]
+# **Library installation**
+
+# %%
+!pip install face_recognition
+!pip install -v --install-option="--no" --install-option="DLIB_USE_CUDA" dlib
+!pip install mtcnn
+
+# %%
+from google.colab import drive
+drive.mount('/content/drive')
+
+# %%
 import face_recognition
 import matplotlib.pyplot as plt
 import cv2
@@ -11,7 +27,11 @@ from matplotlib.patches import Rectangle
 from matplotlib.patches import Circle
 from mtcnn.mtcnn import MTCNN
 
+# %% [markdown]
+# Function that create encoding of all faces from specific folder.
+# Then save that in pkl file in the drive.
 
+# %%
 def savePickleEncodingsToFile(file_name:str, py_obj):
   """
   This function saves a Python Object of any type in Pickle encoded form to a file.
@@ -25,7 +45,7 @@ def savePickleEncodingsToFile(file_name:str, py_obj):
   pickle.dump(py_obj, open_file)
   open_file.close()
 
-
+# %%
 def readPickleEncodingsFromFile(file_name:str):
   """
   This function read a Pickle encoded file and returns the decoded python object.
@@ -40,7 +60,29 @@ def readPickleEncodingsFromFile(file_name:str):
   open_file.close()
   return decode_obj
 
+# %%
+def encodeToPickleEncodings(py_obj):
+  """
+  This function returns Pickle encoded form of Python Object.
+  INPUTS:
+    py_obj:
+      Any Python object which is to be encoded in pickle form.
+  """
+  return pickle.dump(py_obj)
 
+# %%
+def decodeFromPickleEncodings(obj):
+  """
+  This function read a Pickle encoded object and returns the decoded python object.
+  INPUTS:
+    obj:
+      encoded object which is to be decoded.
+  OUPUTS:
+    returns a python object with decoded pickle file
+  """
+  return pickle.load(obj)
+
+# %%
 def faceEncodings(images):
   """
   this function takes a list of images and return list of encoded images.
@@ -56,7 +98,7 @@ def faceEncodings(images):
     encodeList.append(encode)
   return encodeList
 
-
+# %%
 def EncodeFaces(path_to_std_img_dir, std_names_file_name, std_enc_file_name):
   """
   This function encode all the images in a folder and save the encodings in pickel form to the specified folder.
@@ -88,8 +130,18 @@ def EncodeFaces(path_to_std_img_dir, std_names_file_name, std_enc_file_name):
 
   savePickleEncodingsToFile(std_enc_file_name, encodeListKnown)
 
+# %%
+StudentFaces_dir = "/content/drive/MyDrive/ML_Project/Student_Faces"
+Name_list = "/content/drive/MyDrive/ML_Project/StudentName.pkl"
+StudentFaces_Encoding = "/content/drive/MyDrive/ML_Project/StudentEncoding.pkl"
+ClassroomImagePath = "/content/drive/MyDrive/ML_Project/ClassroomPicture/IMG_20220315_095343_1.jpg"
 
-def takeAttendence(enc_names_file, enc_std_faces_file, class_room_img):
+
+# %%
+EncodeFaces(StudentFaces_dir, Name_list, StudentFaces_Encoding)
+
+# %%
+def takeAttendenceFromFiles(enc_names_file, enc_std_faces_file, class_room_img):
   """
   This function detects and recongnize faces in the image of whole class and returns a dict with all the present and absent students 
   INPUTS:
@@ -145,10 +197,10 @@ def takeAttendence(enc_names_file, enc_std_faces_file, class_room_img):
           matchIndex = np.argmin(faceDis)
           match_threshold = np.min(faceDis)
           if faceDis[matchIndex] < 0.50:
-              name = personNames[matchIndex].upper()
+              name = dec_names_lst[matchIndex].upper()
               Name_Dist[name] = [match_threshold,faceLoc]
           else:
-              name = personNames[matchIndex].upper()
+              name = dec_names_lst[matchIndex].upper()
               
               if name not in Not_sure:
                 if name not in Name_Dist:
@@ -176,3 +228,90 @@ def takeAttendence(enc_names_file, enc_std_faces_file, class_room_img):
   return {'total_detected_stds':total_faces,
         'present_stds':list(Name_Dist),
         'absent_stds':list(NOT_SURE_DIST)}
+
+# %%
+takeAttendenceFromFiles(Name_list, StudentFaces_Encoding, ClassroomImagePath)
+
+# %%
+def takeAttendence(dec_names_lst, dec_std_faces, dec_class_room_img):
+  """
+  This function detects and recongnize faces in the image(as numpy array) of whole class and returns a dict with all the present and absent students 
+  INPUTS:
+  dec_names_lst:
+    a lsit of names fo students.
+  dec_std_faces:
+    list of students face encodings (not pickled form) but from face_recog lib.
+  dec_class_room_img:
+    image file of the whole class for attendence in numpy array form.
+  OUTPUTS:
+    it returns the dict object like
+    {
+      'total_detected_stds':21,
+      'present_stds':[{name:'name', id:'id', status:'present'}],
+      'absent_stds':[{name:'name', id:'id', status:'absent'}]
+    }
+  """
+
+  detector = MTCNN()
+
+  faces = detector.detect_faces(dec_class_room_img)
+
+  face_location1 = []
+  for face in faces:
+    x,y,w,h = face['box']
+
+    x1 = y
+    w1 = y+h
+    h1 = x
+    y1 = x+w
+
+    a = (x1, y1, w1, h1)
+    face_location1.append(a)
+
+
+  encodesCurrentFrame = face_recognition.face_encodings(dec_class_room_img, face_location1)
+  total_faces = len(face_location1)
+  print('Total Faces detected: ', total_faces)
+
+  Name_Dist = {}
+  Not_sure = []
+  for encodeFace, faceLoc in zip(encodesCurrentFrame, face_location1):
+          matches = face_recognition.compare_faces(dec_std_faces, encodeFace)
+          
+          faceDis = face_recognition.face_distance(dec_std_faces, encodeFace)
+          matchIndex = np.argmin(faceDis)
+          match_threshold = np.min(faceDis)
+          if faceDis[matchIndex] < 0.50:
+              name = dec_names_lst[matchIndex].upper()
+              Name_Dist[name] = [match_threshold,faceLoc]
+          else:
+              name = dec_names_lst[matchIndex].upper()
+              
+              if name not in Not_sure:
+                if name not in Name_Dist:
+
+                  enc = (name, match_threshold,faceLoc)
+                  Not_sure.append(enc)
+
+  NOT_SURE_DIST = {}
+
+  for Face in Not_sure:
+    
+    Name = Face[0]
+    if Name not in Name_Dist:
+      prop = []
+      prop.append(Face[1])
+      for temp_Face in Not_sure:
+        if Name == temp_Face[0]:
+          prop.append(Face[1])
+      min = np.min(prop)
+
+      for temp in Not_sure:
+        if temp[0] == Name and temp[1] == min:
+          NOT_SURE_DIST[Name] = [temp[1] ,temp[2]]
+
+  return {'total_detected_stds':total_faces,
+        'present_stds':list(Name_Dist),
+        'absent_stds':list(NOT_SURE_DIST)}
+
+
