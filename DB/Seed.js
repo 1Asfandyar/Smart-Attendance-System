0aa7f1e927727ba80f17848a653faab1fa1f5a88
Abdur Rahman <khalil.ahmad1547@gmail.com>
2022-05-29 13:46:10 +0500

    updated Seed.js

--- DB/Seed.js
+++ DB/Seed.js
@@ -2,12 +2,14 @@
 import mongoose from "mongoose";
 import "dotenv/config";
 import { Department } from "./Schema/Department.js";
+import { Section } from "./Schema/Section.js";
 const { MONGODB_LINK } = process.env;
 
 const Seed = async () => {
   // handling the Database link
+  let con;
   try {
-    await mongoose.connect(MONGODB_LINK);
+    con = await mongoose.connect(MONGODB_LINK);
     console.log("Connected to Database");
   } catch (error) {
     console.log("ERROR in conneting to Database");
@@ -16,12 +18,25 @@ const Seed = async () => {
   }
 
   const dept = new Department({
+    name: "BCE",
     department_id: "BCE",
     sections_list: ["A", "B"],
     teachers_list: ["TA", "TB"],
   });
 
-  dept.show();
+  await dept.save();
+  const sec = new Section({
+    name: "A",
+    section_id: "VIII-A",
+    department_id: "BCE",
+    students_list: ["Abdur Rahman", "Abdullah", "Ahmad Zahid"],
+    students_face_encodings:
+      "b'AQAAAAAAAAACAAAAAAAAAAMAAAAAAAAABAAAAAAAAAAFAAAAAAAAAAEAAAAAAAAAAgAAAAAAAAADAAAAAAAAAAQAAAAAAAAABQAAAAAAAAA='",
+    students_names_encodings:
+      "b'QQAAAGIAAABkAAAAdQAAAHIAAAAgAAAAUgAAAGEAAABoAAAAbQAAAGEAAABuAAAAQQAAAGIAAABkAAAAdQAAAGwAAABsAAAAYQAAAGgAAAAAAAAAAAAAAAAAAAAAAAAAQQAAAGgAAABtAAAAYQAAAGQAAAAgAAAAWgAAAGEAAABoAAAAaQAAAGQAAAAAAAAA'",
+  });
+  await sec.save();
+  console.log("done");
 };
 
 Seed();
