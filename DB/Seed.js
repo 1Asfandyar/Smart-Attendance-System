11c43e13ab602c875006335bd722621bb6dccf86
Abdur Rahman <khalil.ahmad1547@gmail.com>
2022-05-29 12:27:35 +0500

    tested department schema

--- DB/Seed.js
+++ DB/Seed.js
@@ -1,1 +1,27 @@
 // this will push some testing data to DB
+import mongoose from "mongoose";
+import "dotenv/config";
+import { Department } from "./Schema/Department.js";
+const { MONGODB_LINK } = process.env;
+
+const Seed = async () => {
+  // handling the Database link
+  try {
+    await mongoose.connect(MONGODB_LINK);
+    console.log("Connected to Database");
+  } catch (error) {
+    console.log("ERROR in conneting to Database");
+    console.log(error);
+    return;
+  }
+
+  const dept = new Department({
+    department_id: "BCE",
+    sections_list: ["A", "B"],
+    teachers_list: ["TA", "TB"],
+  });
+
+  dept.show();
+};
+
+Seed();
